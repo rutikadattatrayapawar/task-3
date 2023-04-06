4e866a6c3c7b50edf45d17f57ff509ca942a7242
Rutika Dattatraya Pawar <127298139+rutikadattatrayapawar@users.noreply.github.com>
2023-04-06 22:04:52 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,49 @@
+// var name = 'Rutika';
+// var age = 22;
+// var school = true;
+// var college = null;
+// var marriage = undefined;
+
+// let students = {
+//     name: 'Rutika',
+//     age: 22,
+//     school: true,
+//     college: null
+// };
+// students.marriage = false;
+// students.college = true
+// if(students.college == false){
+//     console.log(1)
+// }else if(students.college == true){
+//     console.log(2)
+// }
+// else if(students.college == null){
+//     console.log(3)
+// }else{
+//     console.log("College not found.");
+// }
+// switch (students.college) {
+//     case true:
+//         console.log('switch block true')
+//         break;
+//     case false:
+//         console.log('switch block false')
+//         break;
+
+//     case null:
+//         console.log('switch block null')
+//         break;
+
+//     default:
+//         console.log("College not found.");
+//         break;
+// }
+
+
+//Function
+// function clickFunction(){
+//     alert(1);
+// }
+document.getElementById('chatclick').addEventListener("click", function (){
+    alert(1);
+});
